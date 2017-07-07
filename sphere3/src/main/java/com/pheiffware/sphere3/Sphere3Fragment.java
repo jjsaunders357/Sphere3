3f710f23e090fc7d4c6e798cdd667cd48ff901ba
Steve <stephenpheiffer1@gmail.com>
2017-07-07 03:10:36 -0400

    Successfully separated the AndPheiffLib project into a library and demo application module.  The library module can be referenced by other projects, but the demo module can be run as an application.  Additionally, both modules can be viewed/edited/run/rebuilt simultaneously with the Sphere3 project.

---
+++ sphere3/src/main/java/com/pheiffware/sphere3/Sphere3Fragment.java
@@ -0,0 +1,21 @@
+package com.pheiffware.sphere3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.pheiffware.lib.and.gui.graphics.openGL.BaseGameFragment;
+import com.pheiffware.lib.and.gui.graphics.openGL.BaseGameView;
+
+/**
+ * Created by Steve on 7/7/2017.
+ */
+
+public class Sphere3Fragment extends BaseGameFragment
+{
+    @Override
+    public BaseGameView onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return null;
+    }
+}
